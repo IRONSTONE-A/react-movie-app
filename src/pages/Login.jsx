1dfbc86ca189d6b359d7086f30c76f07b003585d
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-07-20 07:22:24 +0300

    register created

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
-
 const Login = () => {
   return (
-    <div>Login</div>
-  )
-}
+    <div className="d-flex justify-content-center">
+      <div className="form-img d-none d-md-block">
+        <img src={"https://picsum.photos/800/800"} alt="sample-movie" />
+      </div>
+      <div className="register-form">
+        <h1 className="form-title display-3 ">Login</h1>
+        <form id="login">
+          <div className="mb-3 ">
+            <label htmlFor="firstName" className="form-label">
+              Email
+            </label>
+            <input
+              type="email"
+              className="form-control"
+              id="email"
+              placeholder="Enter your email adress.."
+              required
+            />
+          </div>
+          <div className="mb-3 ">
+            <label htmlFor="firstName" className="form-label">
+              Password
+            </label>
+            <input
+              type="password"
+              className="form-control"
+              id="password"
+              placeholder="Enter your password.."
+              required
+            />
+          </div>
 
-export default Login
+          <input
+            type="submit"
+            className="btn btn-primary form-control"
+            value="Login"
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
