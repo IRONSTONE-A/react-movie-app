106ca9fc6e7e386fb3ed071077b8e9cfe86c95fd
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-07-20 08:51:21 +0300

    firebase.js updated

--- src/auth/firebase.js
+++ src/auth/firebase.js
@@ -1,5 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
+
 
 // TODO: Replace the following with your app's Firebase project configuration
 // See: https://firebase.google.com/docs/web/learn-more#config-object
@@ -21,3 +23,15 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
+
+export const createUser = asycn(email, password) => {
+    //? yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+    try {
+        let userCredential = await createUserWithEmailAndPassword(auth, email, password)
+        console.log(userCredential);
+    } catch (error) {
+        console.log(err)
+    }
+
+ 
+}
