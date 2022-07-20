6dc81bc61c66586cb6c85657e23a646a2ada18c7
6019thinkingman <99737495+6019thinkingman@users.noreply.github.com>
2022-07-20 06:27:52 +0300

    Navbar cerated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const currentUser = {displayName: "Ironstone A."}
   return (
-    <div>Navbar</div>
-  )
-}
+    <div>
+      <nav className="navbar navbar-expand-lg">
+        <div className="container-fluid">
+          <Link to="/" className="navbar-brand text-white">
+            <h4>Recat Movie App</h4>
+          </Link>
+          <div className="d-flex text-white align-items-center">
+          {currentUser ? (
+            <div>
+            <h5 className="mb-0 text-capitalize">{currentUser.displayName}</h5>
+            <button className="ms-2 btn btn-outline-light">Logout</button>
+            </div>
 
-export default Navbar
+
+          ):null}
+
+
+          </div>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default Navbar;
